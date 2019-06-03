1b6d83ea93a08a5e060252219d8812c033e2120e
Nelson Kamga <kamganelson@gmail.com>
2019-06-03 03:28:27 +0100

    Update `Attachement` and `Connection` components

--- components/index.js
+++ components/index.js
@@ -1,44 +1,57 @@
 const {htm} = require('@zeit/integration-utils');
 
 const Attachement = attachement => htm`
-  <Box minWidth="302px" margin="8px 8px" boxShadow="0 4px 4px rgba(0, 0, 0, 0.12)" background="#fff" padding="16px" borderRadius="8px">
-    <H1>${attachement.addon.name}</H1>
-    <Box margin="12px 0" display="inline" fontSize="12px" border="1px solid #cfd7e6" color="#79589f" background="#f7f8fb" padding="0 5px" fontFamily="consolas,monaco,monospace" borderRadius="4px">${attachement.name}</Box>
-    <Box display="flex" alignItems="center" marginTop="8px">
-      <Box>Connected to </Box>
-      <Box color="#79589f" marginLeft="4px">${attachement.app.name}</Box>
-    </Box>
-    <Box display="flex" justifyContent="space-between" alignItems="center" marginTop="8px">
-      <Link target="_blank" href="${attachement.web_url}">View on Heroku</Link>
-      <Button width="100px" action=${encodeURIComponent(`connect-db-${attachement.id}`)}>Connect</Button>
-    </Box>
+<Box width="100%" margin="20px 0" boxShadow="0 4px 4px rgba(0, 0, 0, 0.12)" background="#fff" padding="32px" borderRadius="8px">
+  <Box display="flex" alignItems="center" justifyContent="space-between">
+    <Box fontSize="24px" fontWeight="700">${attachement.addon.name}</Box>
+    <Link target="_blank" href="${attachement.web_url}">View on Heroku</Link>
   </Box>
+  <Box display="flex" alignItems="center" justifyContent="space-between" marginTop="32px">
+    <Box display="flex">
+      <Box>
+        <Box textTransform="uppercase" color="rgb(136, 136, 136)">Attachement name</Box>
+        <Box fontFamily="consolas,monaco,monospace">${attachement.name}</Box>
+      </Box>
+      <Box width="1px" margin="0 24px" backgroundColor="#ccc"></Box>
+      <Box>
+        <Box textTransform="uppercase" color="rgb(136, 136, 136)">Heroku app</Box>
+        <Box fontFamily="consolas,monaco,monospace">${attachement.app.name}</Box>
+      </Box>
+    </Box>
+    <Button width="100px" action=${encodeURIComponent(`connect-db-${attachement.id}`)}>Connect</Button>
+  </Box>
+</Box>
 `
 
 const Connection = connection => htm`
-  <Box maxWidth="350px" margin="8px 8px" boxShadow="0 4px 4px rgba(0, 0, 0, 0.12)" background="#fff" padding="16px" borderRadius="8px">
-    <H1>${connection.attachement.addon.name}</H1>
-    <Box display="flex" alignItems="center" margin="12px 0" justifyContent="space-between">
-      <Box fontWeight="bold">Env. Variable:</Box>
-      <Box marginLeft="4px" display="inline" fontSize="12px" border="1px solid #cfd7e6" color="#79589f" background="#f7f8fb" padding="2px 5px" fontFamily="consolas,monaco,monospace" borderRadius="4px" lineHeight="1.2">${connection.envVariable}</Box>
-    </Box>
-    <Box display="flex" alignItems="center" margin="12px 0" justifyContent="space-between">
-      <Box fontWeight="bold">Secret:</Box>
-      <Box marginLeft="8px" display="inline" fontSize="12px" border="1px solid #cfd7e6" color="#79589f" background="#f7f8fb" padding="2px 5px" fontFamily="consolas,monaco,monospace" borderRadius="4px" lineHeight="1.2">${connection.secretName}</Box>
-    </Box>
-    <Box display="flex" justifyContent="space-between" alignItems="center" marginTop="8px">
-      <Box display="flex" alignItems="center">
-        <Box backgroundColor="rgb(80, 227, 194)" height="10px" width="10px" borderRadius="6px"></Box>
-        <Box marginLeft="4px">Connected</Box>
-      </Box>
-      <Button themeColor="red" width="100px" action=${encodeURIComponent(`disconnect-db-${connection.id}`)}>Disconnect</Button>
+<Box width="100%" margin="20px 0" boxShadow="0 4px 4px rgba(0, 0, 0, 0.12)" background="#fff" padding="32px" borderRadius="8px">
+  <Box display="flex" alignItems="center" justifyContent="space-between">
+    <Box fontSize="24px" fontWeight="700">${connection.attachement.addon.name}</Box>
+    <Box display="flex" alignItems="center">
+      <Box color="rgb(136, 136, 136)" marginRight="6px">Connected</Box>
+      <Box backgroundColor="rgb(80, 227, 194)" height="10px" width="10px" borderRadius="6px"></Box>
     </Box>
   </Box>
+  <Box display="flex" alignItems="center" justifyContent="space-between" marginTop="32px">
+    <Box display="flex">
+      <Box>
+        <Box textTransform="uppercase" color="rgb(136, 136, 136)">Env. Variable</Box>
+        <Box fontFamily="consolas,monaco,monospace">${connection.envVariable}</Box>
+      </Box>
+      <Box width="1px" margin="0 24px" backgroundColor="#ccc"></Box>
+      <Box>
+        <Box textTransform="uppercase" color="rgb(136, 136, 136)">Secret</Box>
+        <Box fontFamily="consolas,monaco,monospace">${connection.secretName}</Box>
+      </Box>
+    </Box>
+    <Button themeColor="red" width="100px" action=${encodeURIComponent(`disconnect-db-${connection.id}`)}>Disconnect</Button>
+  </Box>
+</Box>
 `
 
 const ConnectionsSection = connections => htm`
-  <Box margin="18px 0">
-    <Box margin="6px 0" marginLeft="8px" fontSize="28px" fontWeight="600">Connections</Box>
+  <Box margin="32px 0 18px">
+    <Box margin="6px 0" fontSize="28px" fontWeight="600">Connections</Box>
     <Box display="flex" flexWrap="wrap" margin="4px 0">
       ${connections.map(connection => Connection(connection))}
     </Box>
@@ -47,8 +60,8 @@ const ConnectionsSection = connections => htm`
 
 const DatastoreHeader = () => htm`
   <Box display="flex" justifyContent="space-between" alignItems="center">
-    <Box margin="6px 0" marginLeft="8px" fontSize="28px" fontWeight="600">Heroku Datastores</Box>
-    <Button action="create-datastore">Create datastore</Button>
+    <Box margin="6px 0" fontSize="28px" fontWeight="600">Heroku Datastores</Box>
+    <Button action="create-datastore" small>Create datastore</Button>
   </Box>
 `
 
@@ -58,7 +71,7 @@ const DatastoreSection = attachements => htm`
       ? DatastoreHeader() 
       : ""
     }
-    <Box display="flex" flexWrap="wrap" margin="4px 0">
+    <Box margin="24px 0">
       ${attachements.map(attachement => Attachement(attachement))}
     </Box>
     ${attachements.length === 0 ? DatastoreEmptyState() : ""}
@@ -73,7 +86,7 @@ const DatastoreEmptyState = () => htm`
   </Box>
 `
 
-const Notice = (data) => htm`
+const Notice = data => htm`
   ${data ? htm`<Notice type="${data.type}">${data.message}</Notice>` : ""}
 `
 
